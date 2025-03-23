59da41a7d125efbe20e51a28a745068b0ec0b1b3
nelson <hejnelson@gmail.com>
2025-03-23 17:58:00 +0100

    fix

--- update_chart.sh
+++ update_chart.sh
@@ -1,10 +1,7 @@
 #! /bin/bash
-
-echo -n "choose chart to update: "
-read chartdirectory
-yaml_file="./$chartdirectory/Chart.yaml" 
+yaml_file="./application-deployment/Chart.yaml" 
 version=$(yq eval '.version' "$yaml_file")
 chartname=$(yq eval '.name' "$yaml_file")
-helm package $chartdirectory
+helm package application-deployment
 mv $chartname-$version.tgz ./docs
 helm repo index ./docs --url https://nelsonjanusson.github.io/portfolio_chart_repo
