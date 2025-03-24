6466b3aee60517c6937a656bba9ccf0db886098a
nelson <hejnelson@gmail.com>
2025-03-24 03:12:16 +0100

    add cilium

--- update_chart.sh
+++ update_chart.sh
@@ -2,6 +2,7 @@
 yaml_file="./application-deployment/Chart.yaml" 
 version=$(yq eval '.version' "$yaml_file")
 chartname=$(yq eval '.name' "$yaml_file")
+helm dependency update ./application-deployment
 helm package application-deployment
 mv $chartname-$version.tgz ./docs
 helm repo index ./docs --url https://nelsonjanusson.github.io/portfolio_chart_repo
